1d8dbfbddbd3b0a04905de9f5b72fc888cf66e7d
Karen Kenward <karen@kenward.org>
2013-11-14 21:46:57 -0600

    Project setup: using npm and bower and yo angular.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ihuChallengeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
