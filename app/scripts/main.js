627f151ea01b72d9a84505e9be5c0490b469d777
Karen Kenward <karen@kenward.org>
2013-11-16 13:20:05 -0600

    Adds title 'click me' using ng-show to switch.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,10 +2,12 @@
 
 angular.module('ihuChallengeApp.main', [])
   .controller('MainCtrl', function($scope) {
+    $scope.layout = 'welcome';
     $scope.greetingMessage = "HELLO!";
 
     $scope.setMessage = function() {
         $scope.greetingMessage = "GOODBYE!";
+        return 'farewell';
     };
   })
   .directive("themessage", function () {
