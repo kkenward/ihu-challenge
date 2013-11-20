1182c458fef081d978b81cee63c56605918928d4
Karen Kenward <karen@kenward.org>
2013-11-19 22:30:17 -0600

    Specify files for jshint.

--- Gruntfile.js
+++ Gruntfile.js
@@ -105,7 +105,8 @@ module.exports = function (grunt) {
       },
       all: [
         'Gruntfile.js',
-        '<%= yeoman.app %>/scripts/{,*/}*.js'
+        '<%= yeoman.app %>/scripts/app.js',
+        '<%= yeoman.app %>/scripts/main.js'
       ]
     },
     coffee: {
